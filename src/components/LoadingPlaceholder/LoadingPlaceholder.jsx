590da61f2948e4d6983536eca05de53d1194f09e
Sajid Hussain <80850448+Sajid-tech@users.noreply.github.com>
2025-08-02 18:33:57 +0530

    Updated LoadingPlaceholder.jsx

---
+++ src/components/LoadingPlaceholder/LoadingPlaceholder.jsx
@@ -0,0 +1,10 @@
+// components/LoadingPlaceholder/LoadingPlaceholder.js
+import React from 'react';
+
+const LoadingPlaceholder = () => (
+  <div style={{ height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+    <div>Loading content...</div>
+  </div>
+);
+
+export default LoadingPlaceholder;
